64768a3dd305c7b2bc75b21c3a8df0cfd3ed0f12
Marcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-21 18:42:51 +0200

    Update Investingcom.py

--- Investingcom.py
+++ Investingcom.py
@@ -4,4 +4,4 @@ df = investpy.get_stock_historical_data(stock='AAPL',
                                         country='United States',
                                         from_date='01/01/2010',
                                         to_date='01/01/2020')
-st.write(df.head())
+st.write(df)
