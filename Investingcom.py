40740280a3237bcb7659adc566e92d3112eb617d
Marcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-21 19:01:54 +0200

    Update Investingcom.py

--- Investingcom.py
+++ Investingcom.py
@@ -7,6 +7,7 @@ df = investpy.get_stock_historical_data(stock='AAPL',
                                         to_date='01/01/2020')
 
 
-sm = investpy.get_stock_countries()
+ls = investpy.get_stock_countries()
+sm = investpy.get_stock_financial_summary(stock='SHP',country='south africa')
 st.write(df)
 st.write(sm)
