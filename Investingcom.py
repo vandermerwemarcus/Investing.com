d523f2df06ef12c3a16a126a097e4750f5ec584f
Marcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-21 18:51:14 +0200

    Update Investingcom.py

--- Investingcom.py
+++ Investingcom.py
@@ -6,3 +6,6 @@ df = investpy.get_stock_historical_data(stock='AAPL',
                                         from_date='01/01/2010',
                                         to_date='01/01/2020')
 st.write(df)
+
+sm = investpy.get_stock_financial_summary(stock='CFRJ',country='South Africa')
+st.write(sm)
